aa32f54fa0576a555f1eabf42aa14d8c54089713
Luiz Felipe Arndt <arndtfelipe@gmail.com>
2018-05-28 00:09:31 -0300

    Criado os teste de integração de Livro e Emprestimo

---
+++ exercicio2/UnitTestProject1/Features/Emprestimos/EmprestimoIntegracaoSistemaTeste.cs
@@ -0,0 +1,137 @@
+﻿using exercicio2.Aplicacao.Features.Emprestimos;
+using exercicio2.Exceptions;
+using exercicio2.Features.Emprestimo;
+using exercicio2.Features.Livros;
+using exercicio2.Infra.Data.Features.Emprestimos;
+using Exercicio2.ComumTeste;
+using Exercicio2.ComumTeste.Base;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.Features.Emprestimos
+{
+    [TestFixture]
+    public class EmprestimoIntegracaoSistemaTeste
+    {
+
+        IEmprestimoRepositorio _IEmprestimoRepositorio;
+
+        EmprestimoService _emprestimoService;
+
+        [SetUp]
+        public void InitializeObjects()
+        {
+            _IEmprestimoRepositorio = new EmprestimoSqlRepositorio();
+            _emprestimoService = new EmprestimoService(_IEmprestimoRepositorio);
+
+            BaseSqlTest.SeedDatabase();
+        }
+
+        [Test]
+        public void Emprestimo_IntegracaoSistema_DeveFuncionar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+            IList<Livro> ListaLivros = new List<Livro>();
+            ListaLivros.Add(livro);
+            Emprestimo Emprestimo = ObjectMother.ObterEmprestimoValido(ListaLivros);
+
+            Emprestimo result = _emprestimoService.Adicionar(Emprestimo);
+
+            result.Id.Should().BeGreaterThan(0);
+
+            IList<Emprestimo> emprestimoList = _emprestimoService.BuscarTodos();
+        }
+
+        [Test]
+        public void Emprestimo_DeleteIntegracaoSistema_DeveFuncionar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+            IList<Livro> ListaLivros = new List<Livro>();
+            ListaLivros.Add(livro);
+            Emprestimo Emprestimo = ObjectMother.ObterEmprestimoValido(ListaLivros);
+
+            _emprestimoService.Deletar(Emprestimo);
+
+            Emprestimo emprestimoGetById = _emprestimoService.BuscarPorId(Emprestimo.Id);
+            emprestimoGetById.Should().BeNull();
+
+            IList<Emprestimo> emprestimoList = _emprestimoService.BuscarTodos();
+            emprestimoList.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void Emprestimo_ObterPorIdIntegracaoSistema_DeveFuncionar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+            IList<Livro> ListaLivros = new List<Livro>();
+            ListaLivros.Add(livro);
+            Emprestimo Emprestimo = ObjectMother.ObterEmprestimoValido(ListaLivros);
+
+            Emprestimo emprestimoGetById = _emprestimoService.BuscarPorId(Emprestimo.Id);
+
+            emprestimoGetById.Should().NotBeNull();
+
+            emprestimoGetById.Id.Should().Be(Emprestimo.Id);
+        }
+
+        [Test]
+        public void Emprestimo_ObterTodosIntegracaoSistema_DeveFuncionar()
+        {
+            IList<Emprestimo> emprestimolist = _emprestimoService.BuscarTodos();
+
+            emprestimolist.Count.Should().Be(1);
+            emprestimolist.First().Id.Should().Be(1);
+        }
+
+
+        [Test]
+        public void Emprestimo_UpdateIdVazioSistema_DeveFalhar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+            IList<Livro> ListaLivros = new List<Livro>();
+            ListaLivros.Add(livro);
+            Emprestimo Emprestimo = ObjectMother.ObterEmprestimoValido(ListaLivros);
+            Emprestimo.Id = 0;
+
+            Action act = () => { _emprestimoService.Atualizar(Emprestimo); };
+
+            act.Should().Throw<IdentifierUndefinedException>();
+        }
+
+        [Test]
+        public void Emprestimo_DeleteIntegracaoSistema_DeveFalhar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+            IList<Livro> ListaLivros = new List<Livro>();
+            ListaLivros.Add(livro);
+            Emprestimo Emprestimo = ObjectMother.ObterEmprestimoValido(ListaLivros);
+
+            Emprestimo.Id = 0;
+
+            Action act = () => { _emprestimoService.Deletar(Emprestimo); };
+            act.Should().Throw<IdentifierUndefinedException>();
+
+            IList<Emprestimo> emprestimoList = _emprestimoService.BuscarTodos();
+            emprestimoList.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Emprestimo_GetEmprestimoIdNull_shouldFail()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+            IList<Livro> ListaLivros = new List<Livro>();
+            ListaLivros.Add(livro);
+            Emprestimo Emprestimo = ObjectMother.ObterEmprestimoValido(ListaLivros);
+
+            Emprestimo.Id = 0;
+
+            Action act = () => { _emprestimoService.BuscarPorId(Emprestimo.Id); };
+            act.Should().Throw<IdentifierUndefinedException>();
+        }
+    }
+}
---
+++ exercicio2/UnitTestProject1/Features/Livros/LivroIntegracaoSistemaTeste.cs
@@ -0,0 +1,118 @@
+﻿using exercicio2.Aplicacao.Features.Livros;
+using exercicio2.Exceptions;
+using exercicio2.Features.Livros;
+using exercicio2.Infra.Data;
+using Exercicio2.ComumTeste;
+using Exercicio2.ComumTeste.Base;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.Features.Livros
+{
+    [TestFixture]
+    public class LivroIntegracaoSistemaTeste
+    {
+
+        ILivroRepositorio _ILivroRepositorio;
+
+        LivroService _livroService;
+
+        [SetUp]
+        public void InitializeObjects()
+        {
+            _ILivroRepositorio = new LivroSqlRepositorio();
+            _livroService = new LivroService(_ILivroRepositorio);
+
+            BaseSqlTest.SeedDatabase();
+        }
+
+        [Test]
+        public void Livro_IntegracaoSistema_DeveFuncionar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+
+            Livro result = _livroService.Adicionar(livro);
+
+            result.Id.Should().BeGreaterThan(0);
+
+            IList<Livro> livroList = _livroService.BuscarTodos();
+        }
+
+        [Test]
+        public void Livro_DeleteIntegracaoSistema_DeveFuncionar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+
+            _livroService.Deletar(livro);
+
+            Livro livroGetById = _livroService.BuscarPorId(livro.Id);
+            livroGetById.Should().BeNull();
+
+            IList<Livro> livroList = _livroService.BuscarTodos();
+            livroList.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void Livro_ObterPorIdIntegracaoSistema_DeveFuncionar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+
+            Livro livroGetById = _livroService.BuscarPorId(livro.Id);
+
+            livroGetById.Should().NotBeNull();
+
+            livroGetById.Id.Should().Be(livro.Id);
+        }
+
+        [Test]
+        public void Livro_ObterTodosIntegracaoSistema_DeveFuncionar()
+        {
+            IList<Livro> livrolist = _livroService.BuscarTodos();
+
+            livrolist.Count.Should().Be(1);
+            livrolist.First().Id.Should().Be(1);
+        }
+
+     
+        [Test]
+        public void Livro_UpdateIdVazioSistema_DeveFalhar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+            livro.Id = 0;
+
+            Action act = () => { _livroService.Atualizar(livro); };
+
+            act.Should().Throw<IdentifierUndefinedException>();
+        }
+
+        [Test]
+        public void Livro_DeleteIntegracaoSistema_DeveFalhar()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+
+            livro.Id = 0;
+
+            Action act = () => { _livroService.Deletar(livro); };
+            act.Should().Throw<IdentifierUndefinedException>();
+
+            IList<Livro> livroList = _livroService.BuscarTodos();
+            livroList.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void Livro_GetLivroIdNull_shouldFail()
+        {
+            Livro livro = ObjectMother.ObterLivroValido();
+
+            livro.Id = 0;
+
+            Action act = () => { _livroService.BuscarPorId(livro.Id); };
+            act.Should().Throw<IdentifierUndefinedException>();
+        }
+    }
+}
